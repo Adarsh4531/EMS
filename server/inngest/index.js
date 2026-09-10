cb4151a5a7ae7dd3465a25933866c07c72e3f81d
Adarsh Singh <adarshsingh4531@gmail.com>
2026-09-11 01:31:18 +0530

    fixed email cron

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -176,9 +176,11 @@ const attendenceReminderCron = inngest.createFunction(
                         `
           })
         })
+        await Promise.all(emailPromises)
+        return{emailsSent: absentEmployees.length}
       })
     }
-    await Promise.all(emailPromises)
+    
     return {totalActive:activeEmployees.length,onLeave:onLeaveIds.length,checkedIn:checkedInIds.length,absent:absentEmployees.length}
 
   }
