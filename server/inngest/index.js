6a01235dd1688e39af3d066662adf0af780009b1
Adarsh Singh <adarshsingh4531@gmail.com>
2026-09-11 13:21:54 +0530

    RECHECKING FIXES

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -122,7 +122,7 @@ const attendenceReminderCron = inngest.createFunction(
     const activeemployees = await step.run("get-active-employees",async()=>{
       const employees = await Employee.find({
         isDeleted: false,
-        employmentStatus: "ACTIVE"
+        employeeStatus: "ACTIVE"
       }).lean()
       return employees.map((e)=>({_id:e._id.toString(),firstName:e.firstName,lastName:e.lastName,email:e.email,department:e.department}))
 
